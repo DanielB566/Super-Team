e1697092f31d0d64cd06feeabdddf8f893bbdf5d
DanielB566 <Danib7382@gmail.com>
2024-05-03 11:55:48 -0400

    Hi

---
+++ scirpt.js
@@ -0,0 +1,55 @@
+const teamMembers = [
+    {
+        name: 'Lionel Messi',
+        position: 'Forward',
+        skills: ['Dribbling', 'Shooting', 'Passing'],
+        strengths: 'Ball Control',
+        weaknesses: 'Heading Ability',
+        biography: 'Lionel Messi is considered one of the best football players ever.'
+    },
+    {
+        name: 'Lionel Messi Jr',
+        position: 'Midfielder',
+        skills: ['Dribbling', 'Shooting', 'Passing'],
+        strengths: 'Ball Control',
+        weaknesses: 'Heading Ability',
+        biography: 'Lionel Messi is considered one of the best football players ever.'
+    },
+]
+
+function generateTeamCards (){
+    const teamCardsContainer = document.getElementById('teamCards')
+
+    teamMembers.forEach(member => {
+        const card = document.createElement('div')
+        card.classList.add('col-md-4')
+
+        //styling cards based on position
+        let backgroundColor = 
+
+        //create list of skills with <li> tags
+        const skillsList = member.skills.map(skill => `<li> ${skill} </li>`).join('')
+            
+       
+
+        card.innerHTML = `
+            <div class = "card">
+            <div class = "card-header"> ${member.name}</div>
+            <div class = "card-body">
+                <p><strong>Position: </strong> ${member.position} </p>
+                <p><strong>Skills: </strong> 
+                <ul>
+                    ${skillsList}
+                </ul>
+                </p>
+                <p><strong>Strengths: </strong> ${member.strengths} </p>
+                <p><strong>Weaknesses: </strong> ${member.weaknesses} </p>
+                <p><strong>Biograpgy: </strong> ${member.biography} </p>
+            </div>
+            </div>
+        `
+        teamCardsContainer.appendChild(card)
+    })
+}
+
+window.onload = generateTeamCards()
